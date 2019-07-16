1ba0fa4283d42b2fe5dc8fd44a9770bb15d5de5e
Valerie Manuel <valerie.manuel11@gmail.com>
2019-07-16 11:44:02 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,6 +1,9 @@
 class EmailParser 
   attr_accessor :name, :csv_emails
   
+  def initialize(csv_emails)
+#   # you don't need name in here, it is redundant/
+#   @name = name
     @csv_emails = csv_emails
   end 
 
